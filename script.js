6c211db21b134707f36aea2d90621208b5a33850
Siod <74300009+Sioood@users.noreply.github.com>
2021-05-29 00:31:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Card 1 Random letter and theme
+
 const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function randomizeLetter() {
@@ -25,6 +27,38 @@ const randomizeTheme = () => {
   randomNumberText.innerText = `${theme[randomizeTheme]}`;
 };
 
+
+//Card 2
+
+const letter2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function randomizeLetter2() {
+  let randomizeLetter2 = Math.floor(Math.random() * letter2.length);
+  randomLetterText2 = document.querySelector("#letter2");
+  randomLetterText2.innerText = `${letter2[randomizeLetter2]}`;
+}
+
+const theme2 = [
+  "Ville",
+  "Pays",
+  "Objet",
+  "Fruit",
+  "Célébrité",
+  "Film",
+  "Musique",
+  "Métier",
+  "Personnage Fictif",
+  "Monument",
+];
+
+const randomizeTheme2 = () => {
+  let randomizeTheme2 = Math.floor(Math.random() * theme2.length);
+  randomNumberText2 = document.querySelector("#theme2");
+  randomNumberText2.innerText = `${theme2[randomizeTheme2]}`;
+};
+
+//Button Click Functions
+
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", randomizeLetter);
@@ -32,6 +66,12 @@ randomizeLetter();
 
 btn.addEventListener("click", randomizeTheme);
 randomizeTheme();
+
+btn.addEventListener("click", randomizeLetter2);
+randomizeLetter2();
+
+btn.addEventListener("click", randomizeTheme2);
+randomizeTheme2();
 
 // const randomLetter = Math.floor(Math.random() * 27);
 
