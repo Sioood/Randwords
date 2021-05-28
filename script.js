5f0410eade393f41c7bc8420c131aac224ce58bf
Siod <74300009+Sioood@users.noreply.github.com>
2021-05-28 19:08:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,32 @@ function randomizeLetter() {
   randomLetterText.innerText = `${letter[randomizeLetter]}`;
 }
 
-const theme = ["Ville", "Pays", "Objet", "Fruit", "Célébrité"];
+const theme = [
+  "Ville",
+  "Pays",
+  "Objet",
+  "Fruit",
+  "Célébrité",
+  "Film",
+  "Musique",
+  "Métier",
+  "Personnage Fictif",
+  "Monument",
+];
 
-function randomizeTheme() {
+const randomizeTheme = () => {
   let randomizeTheme = Math.floor(Math.random() * theme.length);
   randomNumberText = document.querySelector("#theme");
   randomNumberText.innerText = `${theme[randomizeTheme]}`;
-}
+};
 
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", randomizeLetter);
+randomizeLetter();
 
 btn.addEventListener("click", randomizeTheme);
+randomizeTheme();
 
 // const randomLetter = Math.floor(Math.random() * 27);
 
@@ -27,3 +40,17 @@ btn.addEventListener("click", randomizeTheme);
 
 // function randomize() {randomNumberText = document.querySelector('#number');
 // randomNumberText.innerText = `${randomNumber}`
+
+// function randomizeLetter() {
+//   let randomizeLetter = Math.floor(Math.random() * letter.length);
+//   randomLetterText = document.querySelector("#letter");
+//   randomLetterText.innerText = `${letter[randomizeLetter]}`;
+// }
+
+// const theme = ["Ville", "Pays", "Objet", "Fruit", "Célébrité" , "Film", "Musique", "Métier", "Personnage Fictif", "Monument"];
+
+// const randomizeTheme = () => {
+//   let randomizeTheme2 = Math.floor(Math.random() * theme.length);
+//   randomNumberText = document.querySelector("#theme");
+//   randomNumberText.innerText = `${theme[randomizeTheme2]}`;
+// }
